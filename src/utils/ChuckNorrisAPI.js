a53917843396509b77ce106079f541ceea7223d3
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-01-31 15:37:19 -0500

    APIs endpoints.
    Adding Chuck Norris API and Dad Joke API endpoints and searching parameters.

---
+++ src/utils/ChuckNorrisAPI.js
@@ -0,0 +1,13 @@
+/* Author: Sebastian Aguirre Duque
+E-mail: sadw621@gmail.com */
+
+
+export const chuck_API_URL = 'https://api.chucknorris.io/jokes/';
+
+export const randomRequest = 'random';
+
+export const categoriesRequest = 'categories';
+
+export  const freeRequest = 'search?query=';
+
+export const byCategoryRequest = 'random?category=';
---
+++ src/utils/DadJokesAPI.js
@@ -0,0 +1,5 @@
+/* Author: Sebastian Aguirre Duque
+E-mail: sadw621@gmail.com */
+
+
+export const chuck_API_URL = 'https://icanhazdadjoke.com/';
