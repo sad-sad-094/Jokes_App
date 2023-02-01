91ea0112dd22102f6cc0e8ff811d32cf2b556a5b
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-02-01 13:38:39 -0500

    Create MaterialUI media query breakpoints

---
+++ src/styles/Breakpoints.js
@@ -0,0 +1,15 @@
+/* Author: Sebastian Aguirre Duque
+E-mail: sadw621@gmail.com */
+
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 321,
+      tabletPortrait: 480,
+      tabletLandscape: 768,
+      largeScreen: 1024
+    }
+  }
+})
