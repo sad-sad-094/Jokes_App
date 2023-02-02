10ce46743c3d4d7dea3b497c4fff57edf0db96d0
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-02-02 13:49:47 -0500

    Create cards and grid container.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 /* Author: Sebastian Aguirre Duque
 E-mail: sadw621@gmail.com */
 
+import { Typography, ThemeProvider } from '@mui/material';
+
 import './styles/Main.scss';
+import { theme } from './styles/Breakpoints';
+import { Responsive } from './styles/MediaQuery';
 
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Responsive>
+        <div className="cards_container">
+          <div className="card">
+            <div className="card_header">
+              <h3>Source</h3>
+            </div>
+            <div className="card_body">
+              <p>Joke</p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card_header">
+              <h3>Source</h3>
+            </div>
+            <div className="card_body">
+              <p>Joke</p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card_header">
+              <h3>Source</h3>
+            </div>
+            <div className="card_body">
+              <p>Joke</p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card_header">
+              <h3>Source</h3>
+            </div>
+            <div className="card_body">
+              <p>Joke</p>
+            </div>
+          </div>
+          <Typography variant='h1'>
+            Hello, world!
+          </Typography>
+        </div>
+      </Responsive>
+    </ThemeProvider>
   );
 }
 
