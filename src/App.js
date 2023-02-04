f3e175b0a706e2910c867e44c03098a1a0dcbaff
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-02-04 16:00:52 -0500

    Modelate elements, update searching elements and set initial buttons' functions.
    Modulate modal and cards components. Create and update search elements. Create buttons and set its initial functions.

--- src/App.js
+++ src/App.js
@@ -10,115 +10,110 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import { cyan } from '@mui/material/colors';
 // import TextField from '@mui/material/TextField';
 
 import './styles/Main.scss';
 import { theme } from './styles/Breakpoints';
 import { Responsive } from './styles/MediaQuery';
+import Modal from './components/Modal';
+import Cards from './components/Cards';
 
 
 function App() {
 
-  const [hidden, setHidden] = useState('hide');
+  const [hiddenOption, setHiddenOption] = useState('hide');
+  const [hiddenFreeSearch, setHiddenFreeSearch] = useState('hide');
+  const [hiddenSeeCategories, setHiddenSeeCategories] = useState('hide');
 
   return (
 
     <ThemeProvider theme={theme}>
       <Responsive>
 
-        <Box sx={{ '& button': { m: 1 } }}>
+        <div className="search_container">
 
-          <Button size="medium" variant="contained" onClick={() => { setHidden('show') }}>Chuck Norris</Button>
-          <Button size="medium" variant="contained">Dad Joke</Button>
+          <Modal />
 
-        </Box>
+          <Typography variant="h6">Please select a Chuck Norris or Dad joke.</Typography>
 
-        <div className={hidden}>
-          <Box sx={{ '& button': { m: 1 } }} component="form" noValidate autoComplete="on">
+          <Box sx={{ '& button': { m: 1 } }}>
 
-            <InputLabel htmlFor="my-input">Search a joke</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" autoComplete="true" required type="text" />
-            <FormHelperText id="my-helper-text">Please enter a keyword</FormHelperText>
-            {/* <TextField id="outlined-basic" label="Search a joke" variant="outlined" size="normal"
-            margin="dense" helperText="Please enter a keyword" /> */}
-            <Button size="medium" variant="contained">Search</Button>
+            <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} className="buttons" onClick={() => { setHiddenOption('show') }}>Chuck Norris</Button>
+            <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }}>Dad Joke</Button>
 
           </Box>
+
+          <div className={hiddenOption}>
+            <Box sx={{ '& button': { m: 1 } }} className="buttons_container" >
+              <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => { setHiddenOption('hide') }}>Random Search</Button>
+              <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => {
+                setHiddenOption('hide')
+                setHiddenSeeCategories('show')
+              }}>See Categories</Button>
+              <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => {
+                setHiddenOption('hide')
+                setHiddenFreeSearch('show')
+              }}>Free Search</Button>
+            </Box>
+          </div>
+
+          <div className={hiddenFreeSearch}>
+            <Box sx={{ '& button': { m: 1 } }} component="form" noValidate autoComplete="on">
+
+              <div className="chuck_search">
+                <InputLabel htmlFor="my-input">Search a joke</InputLabel>
+                <Input sx={{ width: '25rem' }} id="my-input" aria-describedby="my-helper-text" autoComplete="true" required type="text" />
+                <FormHelperText id="my-helper-text">Please enter a keyword</FormHelperText>
+                {/* <TextField id="outlined-basic" label="Search a joke" variant="outlined" size="normal"
+            margin="dense" helperText="Please enter a keyword" /> */}
+                <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => { setHiddenFreeSearch('hide') }}>Search</Button>
+              </div>
+
+            </Box>
+          </div>
+
+          <div className={hiddenSeeCategories}>
+            <FormControl>
+
+              <div className="categories_container">
+                <FormLabel>Categories</FormLabel>
+
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="animal" control={<Radio />} label="Animal" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="career" control={<Radio />} label="Career" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="Celebrity" control={<Radio />} label="Celebrity" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="dev" control={<Radio />} label="Dev" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="explicit" control={<Radio />} label="Explicit" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="fashion" control={<Radio />} label="Fashion" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="food" control={<Radio />} label="Food" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="fistory" control={<Radio />} label="History" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="money" control={<Radio />} label="Money" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="movie" control={<Radio />} label="Movie" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="music" control={<Radio />} label="Music" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="political" control={<Radio />} label="Political" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="religion" control={<Radio />} label="Religion" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="science" control={<Radio />} label="Science" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="sport" control={<Radio />} label="Sport" />
+                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="travel" control={<Radio />} label="Travel" />
+
+                </RadioGroup>
+
+                <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => { setHiddenSeeCategories('hide') }}>Search</Button>
+              </div>
+
+            </FormControl>
+          </div>
+
         </div>
 
-        <div className={hidden}>
-          <FormControl>
+        <Cards />
 
-            <FormLabel>Categories</FormLabel>
-
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-
-              <FormControlLabel labelPlacement="top" value="animal" control={<Radio />} label="Animal" />
-              <FormControlLabel labelPlacement="top" value="career" control={<Radio />} label="Career" />
-              <FormControlLabel labelPlacement="top" value="Celebrity" control={<Radio />} label="Celebrity" />
-              <FormControlLabel labelPlacement="top" value="dev" control={<Radio />} label="Dev" />
-              <FormControlLabel labelPlacement="top" value="explicit" control={<Radio />} label="Explicit" />
-              <FormControlLabel labelPlacement="top" value="fashion" control={<Radio />} label="Fashion" />
-              <FormControlLabel labelPlacement="top" value="food" control={<Radio />} label="Food" />
-              <FormControlLabel labelPlacement="top" value="fistory" control={<Radio />} label="History" />
-              <FormControlLabel labelPlacement="top" value="money" control={<Radio />} label="Money" />
-              <FormControlLabel labelPlacement="top" value="movie" control={<Radio />} label="Movie" />
-              <FormControlLabel labelPlacement="top" value="music" control={<Radio />} label="Music" />
-              <FormControlLabel labelPlacement="top" value="political" control={<Radio />} label="Political" />
-              <FormControlLabel labelPlacement="top" value="religion" control={<Radio />} label="Religion" />
-              <FormControlLabel labelPlacement="top" value="science" control={<Radio />} label="Science" />
-              <FormControlLabel labelPlacement="top" value="sport" control={<Radio />} label="Sport" />
-              <FormControlLabel labelPlacement="top" value="travel" control={<Radio />} label="Travel" />
-
-            </RadioGroup>
-
-          </FormControl>
-        </div>
-
-        <div className="cards_container">
-          <div className="card">
-            <div className="card_header">
-              <h3>Source</h3>
-            </div>
-            <div className="card_body">
-              <p>Joke</p>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card_header">
-              <h3>Source</h3>
-            </div>
-            <div className="card_body">
-              <p>Joke</p>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card_header">
-              <h3>Source</h3>
-            </div>
-            <div className="card_body">
-              <p>Joke</p>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card_header">
-              <h3>Source</h3>
-            </div>
-            <div className="card_body">
-              <p>Joke</p>
-            </div>
-          </div>
-          <Typography variant='h1'>
-            Hello, world!
-          </Typography>
-        </div>
       </Responsive>
     </ThemeProvider>
 
