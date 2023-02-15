7bac76e908bf60e242882bf86437410f911cb346
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-02-15 15:02:51 -0500

    Create APIs requests' functions

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import { cyan } from '@mui/material/colors';
 // import TextField from '@mui/material/TextField';
+import axios from 'axios';
 
 import './styles/Main.scss';
 import { theme } from './styles/Breakpoints';
 import { Responsive } from './styles/MediaQuery';
 import Modal from './components/Modal';
 import Cards from './components/Cards';
+import { dad_API_Header, dad_API_URL } from './utils/DadJokesAPI';
+import { categoriesRequest, chuck_API_URL, freeRequest, randomRequest } from './utils/ChuckNorrisAPI';
 
 
 function App() {
@@ -25,6 +28,72 @@ function App() {
   const [hiddenOption, setHiddenOption] = useState('hide');
   const [hiddenFreeSearch, setHiddenFreeSearch] = useState('hide');
   const [hiddenSeeCategories, setHiddenSeeCategories] = useState('hide');
+
+  const [keyword, setKeyword] = useState({ keyword: '' });
+  const [dadJoke, setDadJoke] = useState({});
+
+  const getDadJoke = () => {
+
+    const options = {
+      method: 'GET',
+      url: dad_API_URL,
+      headers: dad_API_Header
+    };
+
+    axios.request(options)
+      .then((response) => {
+        setDadJoke(response.data);
+        console.log(response.data.joke); //TODO delete console log
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+
+  const getRandomChuck = () => {
+
+    axios.get(chuck_API_URL + randomRequest)
+      .then((response) => {
+        console.log(response.data.value);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+
+  const getCategoriesChuck = () => {
+
+    axios.get(chuck_API_URL + categoriesRequest)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+
+  const getKeyword = (event) => {
+    event.preventDefault();
+    setKeyword({
+      ...keyword,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const getFreeChuck = () => {
+
+    axios.get(`${chuck_API_URL}${freeRequest}${keyword.keyword}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
 
   return (
 
@@ -40,16 +109,20 @@ function App() {
           <Box sx={{ '& button': { m: 1 } }}>
 
             <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} className="buttons" onClick={() => { setHiddenOption('show') }}>Chuck Norris</Button>
-            <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }}>Dad Joke</Button>
+            <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => { getDadJoke() }}>Dad Joke</Button>
 
           </Box>
 
           <div className={hiddenOption}>
             <Box sx={{ '& button': { m: 1 } }} className="buttons_container" >
-              <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => { setHiddenOption('hide') }}>Random Search</Button>
+              <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => {
+                setHiddenOption('hide')
+                getRandomChuck()
+              }}>Random Search</Button>
               <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => {
                 setHiddenOption('hide')
                 setHiddenSeeCategories('show')
+                getCategoriesChuck()
               }}>See Categories</Button>
               <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => {
                 setHiddenOption('hide')
@@ -63,11 +136,24 @@ function App() {
 
               <div className="chuck_search">
                 <InputLabel htmlFor="my-input">Search a joke</InputLabel>
-                <Input sx={{ width: '25rem' }} id="my-input" aria-describedby="my-helper-text" autoComplete="true" required type="text" />
+
+                <Input
+                  sx={{ width: '25rem' }}
+                  name="keyword" id="my-input"
+                  aria-describedby="my-helper-text"
+                  autoComplete="true"
+                  required
+                  type="text"
+                  label="keyword"
+                  onChange={getKeyword} />
+
                 <FormHelperText id="my-helper-text">Please enter a keyword</FormHelperText>
                 {/* <TextField id="outlined-basic" label="Search a joke" variant="outlined" size="normal"
             margin="dense" helperText="Please enter a keyword" /> */}
-                <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => { setHiddenFreeSearch('hide') }}>Search</Button>
+                <Button size="medium" variant="contained" sx={{ backgroundColor: cyan[600] }} onClick={() => {
+                  setHiddenFreeSearch('hide')
+                  getFreeChuck()
+                }}>Search</Button>
               </div>
 
             </Box>
@@ -85,22 +171,22 @@ function App() {
                   name="row-radio-buttons-group"
                 >
 
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="animal" control={<Radio />} label="Animal" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="career" control={<Radio />} label="Career" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="Celebrity" control={<Radio />} label="Celebrity" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="dev" control={<Radio />} label="Dev" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="explicit" control={<Radio />} label="Explicit" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="fashion" control={<Radio />} label="Fashion" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="food" control={<Radio />} label="Food" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="fistory" control={<Radio />} label="History" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="money" control={<Radio />} label="Money" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="movie" control={<Radio />} label="Movie" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="music" control={<Radio />} label="Music" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="political" control={<Radio />} label="Political" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="religion" control={<Radio />} label="Religion" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="science" control={<Radio />} label="Science" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="sport" control={<Radio />} label="Sport" />
-                  <FormControlLabel sx={{margin: '.5rem'}} labelPlacement="top" value="travel" control={<Radio />} label="Travel" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="animal" control={<Radio />} label="Animal" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="career" control={<Radio />} label="Career" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="Celebrity" control={<Radio />} label="Celebrity" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="dev" control={<Radio />} label="Dev" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="explicit" control={<Radio />} label="Explicit" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="fashion" control={<Radio />} label="Fashion" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="food" control={<Radio />} label="Food" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="fistory" control={<Radio />} label="History" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="money" control={<Radio />} label="Money" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="movie" control={<Radio />} label="Movie" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="music" control={<Radio />} label="Music" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="political" control={<Radio />} label="Political" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="religion" control={<Radio />} label="Religion" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="science" control={<Radio />} label="Science" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="sport" control={<Radio />} label="Sport" />
+                  <FormControlLabel sx={{ margin: '.5rem' }} labelPlacement="top" value="travel" control={<Radio />} label="Travel" />
 
                 </RadioGroup>
 
