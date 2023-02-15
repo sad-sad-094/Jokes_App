2d124753d55a0108ce050241945c31347767ab79
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-02-15 14:20:31 -0500

    Rename variables
    Rename wrong variables names of Dad Joke API endpoints.

--- src/utils/DadJokesAPI.js
+++ src/utils/DadJokesAPI.js
@@ -2,4 +2,6 @@
 E-mail: sadw621@gmail.com */
 
 
-export const chuck_API_URL = 'https://icanhazdadjoke.com/';
+export const dad_API_URL = 'https://icanhazdadjoke.com/';
+
+export const dad_API_Header = {Accept: 'application/json'};
